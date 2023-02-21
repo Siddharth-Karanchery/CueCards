a0a42bcf1365535e8a6ada6d35868fcbd4de99c7
Siddharth-Karanchery <webdev.siddharth@gmail.com>
2023-02-21 20:10:55 +0530

    added questions to QB

--- src/data/QuestionBank/questionBank.js
+++ src/data/QuestionBank/questionBank.js
@@ -1,9 +1,8 @@
 export const questionBankData = [
   {
     id: 1,
-    paperDetails: "[2018, Paper 1, Set C]",
-    question: `The Central Government may, if satisfied that it is necessary that a Patent should be acquired from the Paten-
-    tee for a public purpose within the provision of Patent Act, then a notification is published in:`,
+    paperDetails: "[2018, Paper 1, Set C, Q1]",
+    question: `The Central Government may, if satisfied that it is necessary that a Patent should be acquired from the Patentee for a public purpose within the provision of Patent Act, then a notification is published in:`,
     correctAnswer: "c",
     options: [
       {
@@ -26,7 +25,7 @@ export const questionBankData = [
   },
   {
     id: 2,
-    paperDetails: "[2018, Paper 1, Set C]",
+    paperDetails: "[2018, Paper 1, Set C, Q2]",
     question: ` Choose the correct option from the following:
     Every application for a Patent shall be published under section 11A of the Act except in cases where the application-`,
     correctAnswer: "d",
@@ -51,7 +50,7 @@ export const questionBankData = [
   },
   {
     id: 3,
-    paperDetails: "[2018, Paper 1, Set C]",
+    paperDetails: "[2018, Paper 1, Set C, Q3]",
     question: ` In case of request for permission of making Patent application outside India as per section 39 of the Act the
     time frame to dispose such request by Controller is:`,
     correctAnswer: "c",
@@ -76,7 +75,7 @@ export const questionBankData = [
   },
   {
     id: 4,
-    paperDetails: "[2018, Paper 1, Set C]",
+    paperDetails: "[2018, Paper 1, Set C, Q4]",
     question: ` A Patent awarded by the Patent Office of India is valid in:`,
     correctAnswer: "c",
     options: [
@@ -98,4 +97,554 @@ export const questionBankData = [
       },
     ],
   },
+  {
+    id: 5,
+    paperDetails: "[2018, Paper 1, Set C, Q5]",
+    question: `One PCT application with US priority is filed on 19.09.2009 and for filing National phase application in India the applicant needs to file within:`,
+    correctAnswer: "a",
+    options: [
+      {
+        id: "a",
+        text: `19.04.2012`,
+      },
+      {
+        id: "b",
+        text: `19.09.2014`,
+      },
+      {
+        id: "c",
+        text: `19.09.2013`,
+      },
+      {
+        id: "d",
+        text: ` 19.04.2013`,
+      },
+    ],
+  },
+  {
+    id: 6,
+    paperDetails: "[2018, Paper 1, Set C, Q6]",
+    question: ` A Patent applicant may file a Patent infringement suit within the provision of the Act only after:`,
+    correctAnswer: "b",
+    options: [
+      {
+        id: "a",
+        text: `Publication of the Patent application`,
+      },
+      {
+        id: "b",
+        text: `Grant of the Patent application`,
+      },
+      {
+        id: "c",
+        text: `Priority date of the Patent application`,
+      },
+      {
+        id: "d",
+        text: `Date of filing of Complete Specification`,
+      },
+    ],
+  },
+  {
+    id: 7,
+    paperDetails: "[2018, Paper 1, Set C, Q7]",
+    question: `As per The Patent Act, 1970 the first compulsory licence granted in India to:`,
+    correctAnswer: "a",
+    options: [
+      {
+        id: "a",
+        text: `Natco Pharma on 9th March, 2012`,
+      },
+      {
+        id: "b",
+        text: `Cipla on 9th March, 2009`,
+      },
+      {
+        id: "c",
+        text: `Glenmark on 27th January, 2009`,
+      },
+      {
+        id: "d",
+        text: `None of the above`,
+      },
+    ],
+  },
+  {
+    id: 8,
+    paperDetails: "[2018, Paper 1, Set C, Q8]",
+    question: `Method of committing theft is not Patentable in accordance to one of the Sections of the Patent Act, 1970.
+    Find out the relevant section from the following:`,
+    correctAnswer: "b",
+    options: [
+      {
+        id: "a",
+        text: ` Section 3(c) of the Act`,
+      },
+      {
+        id: "b",
+        text: `Section 3(b) of the Act`,
+      },
+      {
+        id: "c",
+        text: `Section 3(a) of the Act`,
+      },
+      {
+        id: "d",
+        text: `Section 3(d) of the Act`,
+      },
+    ],
+  },
+  {
+    id: 9,
+    paperDetails: "[2018, Paper 1, Set C, Q9]",
+    question: `As per the Patent Act, the term of a Patent of addition filed on 13.04.2012 (granted on 15.01.2015) with re-
+    gard to Complete Specification of an ordinary Patent application filed on 15.07.2008 (granted on 17.09.2011) is
+    up to [considering that neither one is revoked prior to term of validity]`,
+    correctAnswer: "b",
+    options: [
+      {
+        id: "a",
+        text: ` 13.04.2032`,
+      },
+      {
+        id: "b",
+        text: `15.07.2028`,
+      },
+      {
+        id: "c",
+        text: ` 17.09.2031`,
+      },
+      {
+        id: "d",
+        text: `15.01.2035`,
+      },
+    ],
+  },
+  {
+    id: 10,
+    paperDetails: "[2018, Paper 1, Set C, Q10]",
+    question: `An appeal shall not lie to the Appellate Board as per the Patent Act for the following option:`,
+    correctAnswer: "b",
+    options: [
+      {
+        id: "a",
+        text: ` Refusal decision of a Patent application under section 15 of the Act`,
+      },
+      {
+        id: "b",
+        text: `An application is deemed to be abandoned under section 21 of the Act`,
+      },
+      {
+        id: "c",
+        text: `Decision of Controller with regard to compulsory licence under section 84 of the Act.`,
+      },
+      {
+        id: "d",
+        text: `The decision related to post grant opposition under section 25(4) of the Act`,
+      },
+    ],
+  },
+  {
+    id: 11,
+    paperDetails: "[2018, Paper 1, Set C, Q11]",
+    question: `The period for which an application for Patent shall not ordinarily be open to public in Patent Office Journal
+    from the date of filing or date of priority of the application, whichever is earlier as per the provision of the Act
+    (Assume that there is no request for early publication)`,
+    correctAnswer: "a",
+    options: [
+      {
+        id: "a",
+        text: `18 months`,
+      },
+      {
+        id: "b",
+        text: `12 months`,
+      },
+      {
+        id: "c",
+        text: `6 months`,
+      },
+      {
+        id: "d",
+        text: `8 months`,
+      },
+    ],
+  },
+  {
+    id: 12,
+    paperDetails: "[2018, Paper 1, Set C, Q12]",
+    question: ` An Indian applicant has filed an ordinary Patent application before Patent Office by filing Provisional Spec-
+    ification on 12.05.2011 (assuming no postdating of application). Find out the correct option from the following:`,
+    correctAnswer: "b",
+    options: [
+      {
+        id: "a",
+        text: `The applicant filed the Complete Specification on 12.8.2012 after filing Form 4 on 11.05.2012 for three months
+        extension`,
+      },
+      {
+        id: "b",
+        text: `The applicant filed Complete Specification on 12.05.2012`,
+      },
+      {
+        id: "c",
+        text: `The applicant filed a petition under 138 of the prevailing Rules and submitted the Complete Specification on
+        12.06.2012`,
+      },
+      {
+        id: "d",
+        text: `None of the above`,
+      },
+    ],
+  },
+  {
+    id: 13,
+    paperDetails: "[2018, Paper 1, Set C, Q13]",
+    question: `  As per The Patent Act, the qualification required for a Registered Patent Agent to work as the Technical
+    Member of the Appellate Board is:`,
+    correctAnswer: "b",
+    options: [
+      {
+        id: "a",
+        text: `Master degree in Science with at least 8 years functioned as a Registered Patent Agent`,
+      },
+      {
+        id: "b",
+        text: `Degree in Engineering or technology or Master degree in Science with at least 10 years functioned as a Regis-
+        tered Patent Agent`,
+      },
+      {
+        id: "c",
+        text: `Degree in Engineering or technology or Master degree in Science with at least 10 years functioned as a Regis-
+        tered Patent Agent`,
+      },
+      {
+        id: "d",
+        text: `All of the above`,
+      },
+    ],
+  },
+  {
+    id: 14,
+    paperDetails: "[2018, Paper 1, Set C, Q14]",
+    question: ` As per the Patent Act, Register of Patents shall contain:`,
+    correctAnswer: "d",
+    options: [
+      {
+        id: "a",
+        text: `Names and addresses of grantees of Patents`,
+      },
+      {
+        id: "b",
+        text: `Revocations of Patents`,
+      },
+      {
+        id: "c",
+        text: `Notification of Assignments and of transmissions of Patents`,
+      },
+      {
+        id: "d",
+        text: `All of the above`,
+      },
+    ],
+  },
+  {
+    id: 15,
+    paperDetails: "[2018, Paper 1, Set C, Q15]",
+    question: ` Any person interested may make an application to the Controller for granting compulsory licence on Patent
+    as per The Patent Act, in the following grounds:`,
+    correctAnswer: "a",
+    options: [
+      {
+        id: "a",
+        text: ` That the Patented invention is not available to the public at reasonably affordable price`,
+      },
+      {
+        id: "b",
+        text: `That the Patented invention is reserved for noncommercial purpose`,
+      },
+      {
+        id: "c",
+        text: ` That the Patented invention is imported with immediate effect`,
+      },
+      {
+        id: "d",
+        text: ` None of the above`,
+      },
+    ],
+  },
+  {
+    id: 16,
+    paperDetails: "[2018, Paper 1, Set C, Q16]",
+    question: `A researcher finds an idea on recycling of biodegradable plastic. How does he proceed for patenting his
+    idea?`,
+    correctAnswer: "b",
+    options: [
+      {
+        id: "a",
+        text: ` He needs to publish in the Scientific Journal and has to wait for invitation from Patent Office`,
+      },
+      {
+        id: "b",
+        text: `He needs to submit Provisional Specification and subsequently to file Complete Specification before Patent
+        Office`,
+      },
+      {
+        id: "c",
+        text: ` Both A and B`,
+      },
+      {
+        id: "d",
+        text: ` None of the above`,
+      },
+    ],
+  },
+  {
+    id: 17,
+    paperDetails: "[2018, Paper 1, Set C, Q17]",
+    question: `The “invention” as per Patent Act, 1970 incorporates:`,
+    correctAnswer: "d",
+    options: [
+      {
+        id: "a",
+        text: `Capable of Industrial application`,
+      },
+      {
+        id: "b",
+        text: `An inventive step`,
+      },
+      {
+        id: "c",
+        text: `A new product or process`,
+      },
+      {
+        id: "d",
+        text: ` All of the above`,
+      },
+    ],
+  },
+  {
+    id: 18,
+    paperDetails: "[2018, Paper 1, Set C, Q18]",
+    question: ` Identify from the following patented products, for which compulsory licence are available for export which
+    is entitled for manufacture and export to any country having insufficient / no manufacturing capacity to address
+    public health problems as per The Patent Act:`,
+    correctAnswer: "b",
+    options: [
+      {
+        id: "a",
+        text: ` Disaster management related device`,
+      },
+      {
+        id: "b",
+        text: `Pharmaceutical products`,
+      },
+      {
+        id: "c",
+        text: ` Food products`,
+      },
+      {
+        id: "d",
+        text: ` All of the above`,
+      },
+    ],
+  },
+  {
+    id: 19,
+    paperDetails: "[2018, Paper 1, Set C, Q19]",
+    question: ` When a Patent completes its full term of 20 years within the provision of The Patent Act, it:`,
+    correctAnswer: "d",
+    options: [
+      {
+        id: "a",
+        text: ` May be renewed for a further period of 10 years`,
+      },
+      {
+        id: "b",
+        text: `May be renewed only under special circumstances`,
+      },
+      {
+        id: "c",
+        text: ` May be renewed for a period of 5 years`,
+      },
+      {
+        id: "d",
+        text: ` Shall not be renewed`,
+      },
+    ],
+  },
+  {
+    id: 20,
+    paperDetails: "[2018, Paper 1, Set C, Q20]",
+    question: ` A ceased Patent due to non-payment of renewal fees, may be restored by filing an application for resto-
+    ration with prescribed fee as per The Patent Act:`,
+    correctAnswer: "b",
+    options: [
+      {
+        id: "a",
+        text: `Within 12 months from the date on which Patent ceased to have effect`,
+      },
+      {
+        id: "b",
+        text: `Within 18 months from the date on which Patent ceased to have effect`,
+      },
+      {
+        id: "c",
+        text: ` Within 48 months from the date on which Patent ceased to have effect`,
+      },
+      {
+        id: "d",
+        text: ` None of the above`,
+      },
+    ],
+  },
+  {
+    id: 21,
+    paperDetails: "[2018, Paper 1, Set C, Q21]",
+    question: ` The submission of proof of right to make an application for a Patent made by virtue of an assignment as
+    per the Act shall be within a prescribed period of:`,
+    correctAnswer: "c",
+    options: [
+      {
+        id: "a",
+        text: `With the application or within 3 months from date of application`,
+      },
+      {
+        id: "b",
+        text: `With the application or 9 months from date of application`,
+      },
+      {
+        id: "c",
+        text: ` With the application or 6 months from date of application`,
+      },
+      {
+        id: "d",
+        text: ` With the application or 12 months from date of application`,
+      },
+    ],
+  },
+  {
+    id: 22,
+    paperDetails: "[2018, Paper 1, Set C, Q22]",
+    question: ` Find out from the following which is not the ground for opposition under Section 25(1) of Patent Act, 1970:`,
+    correctAnswer: "a",
+    options: [
+      {
+        id: "a",
+        text: `That the request for examination pursuant to the filing of Complete Specification has not been filed by the
+        applicant himself`,
+      },
+      {
+        id: "b",
+        text: `That the Complete Specification does not sufficiently and clearly describe the invention or the method by
+        which it is to be performed`,
+      },
+      {
+        id: "c",
+        text: ` That the applicant has failed to disclose to the Controller the information required by section 8 or has fur-
+        nished the information which in any material particular was false to his knowledge`,
+      },
+      {
+        id: "d",
+        text: ` That the Patentee or the person under or through whom he claims, wrongfully obtained the invention or any
+        part thereof from him or from a person under or through whom he claims`,
+      },
+    ],
+  },
+  {
+    id: 23,
+    paperDetails: "[2018, Paper 1, Set C, Q23]",
+    question: ` International application as per the Patent Act, 1970 means:`,
+    correctAnswer: "c",
+    options: [
+      {
+        id: "a",
+        text: `An application filed in WTO`,
+      },
+      {
+        id: "b",
+        text: `An application filed under Hague agreement`,
+      },
+      {
+        id: "c",
+        text: ` An application for Patent made in accordance with the Patent Cooperation Treaty`,
+      },
+      {
+        id: "d",
+        text: ` An application for Patent made in accordance with the Budapest Treaty`,
+      },
+    ],
+  },
+  {
+    id: 24,
+    paperDetails: "[2018, Paper 1, Set C, Q24]",
+    question: ` Discovery of a new galaxy is not Patentable:`,
+    correctAnswer: "a",
+    options: [
+      {
+        id: "a",
+        text: `As per section 3(c) of The Patent Act, 1970`,
+      },
+      {
+        id: "b",
+        text: `As per section 3(k) of The Patent Act, 1970`,
+      },
+      {
+        id: "c",
+        text: `As per section 3(d) of The Patent Act, 1970`,
+      },
+      {
+        id: "d",
+        text: ` As per section 3(b) of The Patent Act, 1970`,
+      },
+    ],
+  },
+  {
+    id: 25,
+    paperDetails: "[2018, Paper 1, Set C, Q25]",
+    question: ` Definition of “Capable of industrial application”, in relation to an invention as per The Patent Act means:`,
+    correctAnswer: "a",
+    options: [
+      {
+        id: "a",
+        text: ` That the invention is capable of being made or used in an industry`,
+      },
+      {
+        id: "b",
+        text: `That the invention is capable of being manufactured in an industry`,
+      },
+      {
+        id: "c",
+        text: `That the invention is capable of being sold by an industry`,
+      },
+      {
+        id: "d",
+        text: `None of the above`,
+      },
+    ],
+  },
 ];
+// {
+//   id: 7,
+//   paperDetails: "[2018, Paper 1, Set C]",
+//   question: `As per The Patent Act, 1970 the first compulsory licence granted in India to:`,
+//   correctAnswer: "a",
+//   options: [
+//     {
+//       id: "a",
+//       text: `Natco Pharma on 9th March, 2012`,
+//     },
+//     {
+//       id: "b",
+//       text: `Cipla on 9th March, 2009`,
+//     },
+//     {
+//       id: "c",
+//       text: `Glenmark on 27th January, 2009`,
+//     },
+//     {
+//       id: "d",
+//       text: `None of the above`,
+//     },
+//   ],
+// },
